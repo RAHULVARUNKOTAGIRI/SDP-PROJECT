e977d7d89c62fb2aede68c1ed5fa2d23514eb3ba
Rahul Varun Kotagiri <rahulvarun9441@gmail.com>
2026-05-03 00:05:56 +0530

    final changes before deploy

--- backend/main.py
+++ backend/main.py
@@ -35,10 +35,7 @@ app = FastAPI(title="Diabetic Retinopathy Detection API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-    ],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
